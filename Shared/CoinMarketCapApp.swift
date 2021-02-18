8ba091df3a90b114bfa87555d2d1638627e01a6a
Dima Yarmolchuk <yarmolchuk@gmail.com>
2021-02-18 23:23:32 +0200

    Initial Commit

---
+++ Shared/CoinMarketCapApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinMarketCapApp.swift
+//  Shared
+//
+//  Created by Dima Yarmolchuk on 18.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct CoinMarketCapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
